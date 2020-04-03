67948bd92028e55004c752793b7b8633f39b8331
Stas Globus <stani246@gmail.com>
2020-04-04 00:15:01 +0300

    working, more job needed

---
+++ DNS-SERVER/DNS-SERVER/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+#include <windows.h>
+#include "conio.h"
+#pragma comment(lib, "ws2_32.lib")
+#endif
